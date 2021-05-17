4221b18fdd9b607e8f338ff3f9c3cb77b66605e4
Capucine Destaing <capucine.destaing@polytechnique.edu>
2021-05-17 14:44:32 +0200

    Initial Push

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='yourpackage-run',
+setup(name='codedecapu',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='yourpackage-run',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/yourpackage-run-run'],
+      scripts=['scripts/codedecapu-run'],
       zip_safe=False)
